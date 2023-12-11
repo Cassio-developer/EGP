fc50efefc463f9ccdcd0f535ee26f009b6ef1dea
Guilherme Hallmann <guilhermehallmann@gmail.com>
2023-12-11 13:10:15 -0300

    feat: criação navlink

---
+++ resources/js/Components/Navlink.vue
@@ -0,0 +1,8 @@
+<template>
+    <Link class="hover:bg-gray-400 rounded py-2 px-4 mx-2">
+        <slot />
+    </Link>
+</template>
+<script setup>
+import { Link } from "@inertiajs/vue3";
+</script>

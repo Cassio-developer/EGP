6426157ffafb6b6d5e15f17b9a46c91cc4857183
Guilherme Hallmann <guilhermehallmann@gmail.com>
2023-12-14 11:18:47 -0300

    feat: criação de layout padrão

---
+++ resources/js/Pages/Shared/Layout.vue
@@ -0,0 +1,13 @@
+<template>
+    <section>
+        <Header />
+        <Navbar />
+    </section>
+    <section>
+        <slot />
+    </section>
+</template>
+<script setup>
+import Header from "../../Components/Header.vue";
+import Navbar from "../../Components/Navbar.vue";
+</script>

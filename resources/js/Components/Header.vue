64bc8da769f63f6bf3e3031d73332c9a2b69b2a8
Guilherme Hallmann <guilhermehallmann@gmail.com>
2023-12-14 11:21:45 -0300

    ref: ajuste responsividade header

---
+++ resources/js/Components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <section class="bg-white font-family-karla">
+        <!-- Top Bar Nav -->
+        <nav class="w-full py-2 bg-blue-800 shadow">
+            <div
+                class="w-full container mx-auto flex flex-wrap items-center justify-between"
+            >
+                <div
+                    class="flex items-center text-lg no-underline text-white space-x-4 mx-4"
+                >
+                    <a
+                        href="https://www.facebook.com/escoladegestaopublicasaoleopoldo"
+                        target="_blank"
+                    >
+                        <i class="fab fa-facebook"></i>
+                    </a>
+                    <a href="#">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                </div>
+            </div>
+        </nav>
+
+        <header class="w-full container mx-auto">
+            <div class="flex flex-col items-center">
+                <img src="/img/logoEGP.png" class="w-1/3" />
+            </div>
+        </header>
+    </section>
+</template>
+
+<script setup></script>

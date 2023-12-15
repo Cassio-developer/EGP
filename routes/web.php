ea745deba7be722e2dbb308100bdda4eca72a949
Guilherme Hallmann <guilhermehallmann@gmail.com>
2023-12-15 10:00:01 -0300

    feat: add rotas para noticias

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,30 @@
 <?php
 
+use Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return Inertia::render('Home');
+});
+
+
+Route::prefix('noticias')->group(function () {
+    Route::get('/all', [NoticiaController::class, 'index']);
+    Route::get('/create', [NoticiasController::class, 'create']);
+    Route::post('/', [NoticiaController::class, 'store'])->name('salvar-noticia')->middleware([HandlePrecognitiveRequests::class]);
+    // Route::get('/show/{id}', [NoticiaController::class, 'show']);
+    Route::get('/show', function () {
+        return Inertia::render('Noticia/Show');
+    });
+    Route::get('/edit/{id}', [NoticiaController::class, 'edit']);
+    Route::put('/edit/{id}', [NoticiaController::class, 'update'])->name('atualizar-noticia')->middleware([HandlePrecognitiveRequests::class]);
+    Route::delete('/delete/{id}', [NoticiaController::class, 'destroy'])->name('excluir-noticia')->middleware([HandlePrecognitiveRequests::class]);
+});
+
+Route::get('/noticia', function () {
+    return Inertia::render('Noticia/Show');
 });
 
 //Exemplo header e card template tailwind awesome

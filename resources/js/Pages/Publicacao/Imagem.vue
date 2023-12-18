bc44d740a5c64cbc7660309e54c6488817493566
Guilherme Hallmann <guilhermehallmann@gmail.com>
2023-12-18 11:04:53 -0300

    ref: mudança para publicacao e componentizacao de elementos da pagina

---
+++ resources/js/Pages/Publicacao/Imagem.vue
@@ -0,0 +1,11 @@
+<template>
+    <img class="w-full" :src="imagemUrl" :alt="legenda" />
+    <p class="text-xs italic">{{ legenda }}</p>
+</template>
+
+<script setup>
+const props = defineProps({
+    imagemUrl: String,
+    legenda: String,
+});
+</script>
